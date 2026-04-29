12a5365b02e237c6ea7bca8e65b7e8c7fc56dd86
Katelyn Rohrer <k.rohrer03@gmail.com>
2026-04-28 17:20:34 -0700

    made delete and the empty files for other baseline ops

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
 IMAGE_NAME = pdp_container
 
-.PHONY: build run run-full run-baseline run-baseline-full track-pdp track-pdp-full track-baseline track-baseline-full
-
 build:
 	docker build -t $(IMAGE_NAME) .
 
@@ -16,15 +14,23 @@ run-full:
 	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'python3 -u src/main.py'
 
 
-run-baseline:
-	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline.py'
-
 run-baseline-sort:
 	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/sort.py'
 
 run-baseline-insert:
 	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/insert.py'
 
+run-baseline-delete:
+	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/delete.py'
 
-run-baseline-full:
-	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'python3 -u src/baseline.py'
+run-baseline-lookup:
+	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/lookup.py'
+
+run-baseline-filter:
+	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/filter.py'
+
+run-baseline-project:
+	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/project.py'
+
+run-baseline-count:
+	docker run --rm -it -v "$$(pwd)":/work -w /work $(IMAGE_NAME) bash -lc 'ulimit -v 1048576 && python3 -u src/baseline/count.py'
--- src/baseline/delete.py
+++ src/baseline/delete.py
@@ -19,7 +19,7 @@ DATA_FILES = [
 
 
 def main():
-    char_inserted = "99999999999999999999999999999"
+    key_to_delete = "7zzwQwN3jNiK46B2M9kL2Q"
     start = time.time()
 
     file = DATA_FILES[3]
@@ -27,51 +27,48 @@ def main():
     mem, _ = resource.getrlimit(resource.RLIMIT_AS)  # virtual limits (e.g. the 1gb limit)
     if mem == -1:
         # in real use case, this would always be used by default
-        mem = psutil.virtual_memory().available # physical limits (e.g container capacity)
+        mem = psutil.virtual_memory().available  # physical limits (e.g container capacity)
     size_limit = mem / 15  # pandas typically needs 2-5x the space of the file. we're being conservative here
 
     sample_df = pd.read_csv(file, dtype=str, nrows=1000)
     columns = sample_df.columns
     row_size = sample_df.memory_usage(deep=True).sum() / len(sample_df)
     col = columns[0]
+    chunk_size = max(1, int(size_limit / row_size))
+
+    folder = os.path.dirname(file)
+    name = os.path.basename(file)
+    root, ext = os.path.splitext(name)
 
     ## SORTING ##
     # requires several temp files because
     # sorting a CSV larger than memory
     # cannot be done in-place
 
-    ## SORTED INSERT ##
+    ## SORTED DELETE ##
     # ** requires one temp file **
-    row = {k: char_inserted for k in columns}
-    chunk_size = max(1, int(size_limit / row_size))
-
-    folder = os.path.dirname(file)
-    name = os.path.basename(file)
-    root, ext = os.path.splitext(name)
-    output_file = os.path.join(folder, f"{root}_inserted{ext}")
-    temp_file = os.path.join(folder, f".{root}_inserted{ext}.tmp")
+    output_file = os.path.join(folder, f"{root}_deleted{ext}")
+    temp_file = os.path.join(folder, f".{root}_deleted{ext}.tmp")
 
     columns = pd.read_csv(file, dtype=str, nrows=0).columns
-    row_df = pd.DataFrame([row], columns=columns)
-    inserted = False
+    deleted = False
 
     with open(temp_file, "w", newline="") as out:
         pd.DataFrame(columns=columns).to_csv(out, index=False)
 
         for df in pd.read_csv(file, dtype=str, chunksize=chunk_size):
-            if not inserted and row[col] <= df[col].iloc[-1]:
-                before = df[df[col] < row[col]]
-                after = df[df[col] >= row[col]]
+            if not deleted:
+                matches = df[col] == key_to_delete
 
-                before.to_csv(out, header=False, index=False)
-                row_df.to_csv(out, header=False, index=False)
-                after.to_csv(out, header=False, index=False)
-                inserted = True
+                if matches.any():
+                    delete_idx = matches.idxmax()
+                    keep_df = df.drop(index=delete_idx)
+                    keep_df.to_csv(out, header=False, index=False)
+                    deleted = True
+                else:
+                    df.to_csv(out, header=False, index=False)
             else:
                 df.to_csv(out, header=False, index=False)
-
-        if not inserted:
-            row_df.to_csv(out, header=False, index=False)
 
     os.replace(temp_file, output_file)
 
@@ -81,13 +78,15 @@ def main():
     end = time.time()
 
     output = f"File: {file}\n" + \
-             f"Inserted: {char_inserted}\n" + \
+             f"Deleted: {key_to_delete}\n" + \
+             f"Found: {deleted}\n" + \
              f"Time: {(end - start) * 1000:.2f} ms\n\n"
 
     print(output)
 
-    with open("./metrics/baseline_1gb_insert.txt", "a") as file:
+    with open("./metrics/baseline_1gb_delete.txt", "a") as file:
         file.write(output)
+
 
 if __name__ == "__main__":
     main()
